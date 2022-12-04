a2b154ec524f795d1b1ea88affac3037713d8a97
BellGaleano2001 <milagrosgalea.10@gmail.com>
2022-12-04 19:07:07 -0300

    Correción de rutas

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\SolicitanteController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::apiResource('solicitante', SolicitanteController::class)->middleware('auth:api');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,47 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Enrutado(Controllers-Funcionalidades)
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [\App\Http\Controllers\CustomAuthController::class, 'index'])->name('login');
+Route::post('custom-login', [\App\Http\Controllers\CustomAuthController::class, 'customLogin'])->name('login.custom'); 
+Route::get('registration', [\App\Http\Controllers\CustomAuthController::class, 'registration'])->name('register-user');
+Route::post('custom-registration', [\App\Http\Controllers\CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
+Route::get('signout', [\App\Http\Controllers\CustomAuthController::class, 'signOut'])->name('signout');
+Route::get('update', [\App\Http\Controllers\UserPasswordController::class, 'show']);
+Route::post('update', [\App\Http\Controllers\UserPasswordController::class, 'update'])->name('register.update'); 
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('herramienta', App\Http\Controllers\HerramientaController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',
+    ]);
+    Route::resource('bocaagua', App\Http\Controllers\BocaAguaController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',
+    ]);
+    Route::resource('vehiculo', App\Http\Controllers\VehiculoController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',
+    ]);
+    Route::resource('insumo', App\Http\Controllers\InsumoController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',
+    ]);
+    Route::resource('estadoherramienta', App\Http\Controllers\EstadoHerramientaController::class)->only([
+        'index']);
+    Route::resource('menu', App\Http\Controllers\MenuController::class)->only([
+        'index']);
+    Route::resource('personal', App\Http\Controllers\PersonalController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',]);    
+    Route::resource('siniestro', App\Http\Controllers\SiniestroController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',
+    ]);   
+    Route::resource('servicio', App\Http\Controllers\ServicioController::class)->only([
+        'index', 'show', 'create', 'store', 'edit', 'update', 'destroy',
+    ]);  
+    Route::get('dashboard', [\App\Http\Controllers\CustomAuthController::class, 'dashboard']); 
+
+});
+
+
