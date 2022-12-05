71481b5e0a76e0b49bd9d9a830ba1dfee4d26106
AzulAlderete <azulthaliaalderete@gmail.com>
2022-12-05 15:29:06 -0300

    Cambios en database

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,3 +26,18 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
+
